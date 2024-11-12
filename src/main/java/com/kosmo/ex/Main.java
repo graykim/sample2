b1183e057222f41b61b0aa02ab1d1ea11d4c0720
KOSMO) <qca1313@naver.com>
2024-11-12 10:47:34 +0900

    오재헌의 세번째 커밋

--- src/main/java/com/kosmo/ex/Main.java
+++ src/main/java/com/kosmo/ex/Main.java
@@ -3,6 +3,6 @@ package com.kosmo.ex;
 public class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요");
-        System.out.println("이것은 오재헌이 만든 로직");
+        System.out.println("이것은 오재헌이 만든 로직2222");
     }
 }
